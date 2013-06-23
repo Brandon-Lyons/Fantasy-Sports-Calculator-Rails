4185803b191675fcde01884cec34836d196eb8fc
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-06-22 22:42:23 -0500

    added kicker attributes to league model

--- app/models/football_league.rb
+++ app/models/football_league.rb
@@ -1,5 +1,5 @@
 class FootballLeague < ActiveRecord::Base
-  attr_accessible :name, :pass_td, :pass_yards, :int, :pass_2pt, :rush_td, :rush_yards, :rush_2pt, :rec_td, :rec_yards, :rec_2pt
+  attr_accessible :name, :pass_td, :pass_yards, :int, :pass_2pt, :rush_td, :rush_yards, :rush_2pt, :rec_td, :rec_yards, :rec_2pt, :fg_made, :fg_missed, :pat_made
   has_many :user_football_leagues, :dependent => :delete_all
   has_many :users, through: :user_football_leagues
   validates :name, :presence => true
