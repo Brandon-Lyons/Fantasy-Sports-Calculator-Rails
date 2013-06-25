b5147ea1f5055cdc8eea162a9e2849314ba146b3
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-06-25 14:34:08 -0500

    added mesaage for when no year or month is entered

--- lib/interface.rb
+++ lib/interface.rb
@@ -23,22 +23,26 @@ module Interface
   end
 
   def player_stats(year, week, player_id)
-    uri = URI("http://api.nfldata.apiphany.com/developer/JSON/PlayerGameStatsByPlayerID/#{year}/#{week}/#{player_id}")
-
-    uri.query = URI.encode_www_form({
-      'key' => '1dca78d0-2f19-4954-863a-338f4f2ab105',
-      })
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-
-    response = Net::HTTP::start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-      http.request(request)
-    end
-
-    if response.body.empty?
-      stats = {"error" => "Sorry there are no stats to show! Please go back and enter a valid week and/or year. This sometimes happens if the player is on a bye week."}
+    if year.empty? || week.empty?
+      stats = {"error" => "A year or week is needed to calculate."}
     else
-      stats = JSON.parse(response.body)
+      uri = URI("http://api.nfldata.apiphany.com/developer/JSON/PlayerGameStatsByPlayerID/#{year}/#{week}/#{player_id}")
+
+      uri.query = URI.encode_www_form({
+        'key' => '1dca78d0-2f19-4954-863a-338f4f2ab105',
+        })
+
+      request = Net::HTTP::Get.new(uri.request_uri)
+
+      response = Net::HTTP::start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
+      end
+
+      if response.body.empty?
+        stats = {"error" => "Sorry there are no stats to show! Please go back and enter a valid week and/or year. This sometimes happens if the player is on a bye week."}
+      else
+        stats = JSON.parse(response.body)
+      end
     end
 
     stats
