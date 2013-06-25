482eef2c034507bc6ea0a36f8872eca0470a8d5c
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-06-25 10:32:06 -0500

    added carousel to home page

--- app/models/football_player.rb
+++ app/models/football_player.rb
@@ -20,61 +20,36 @@ class FootballPlayer < ActiveRecord::Base
   end
 
   def pass_score(stats, league)
-    td = stats["PassingTouchdowns"].to_i
-    yards = stats["PassingYards"].to_i
-    int = stats["PassingInterceptions"].to_i
-    two_pt = stats["TwoPointConversionPasses"].to_i
-    td_rule = league.pass_td
-    yard_rule = league.pass_yards
-    int_rule = league.int
-    two_pt_rule = league.pass_2pt
-    td_score = td * td_rule
-    yards_score = (yards / 25) * yard_rule
-    int_score = int * int_rule
-    two_pt_score = two_pt * two_pt_rule
+    td_score = stats["PassingTouchdowns"].to_i * league.pass_td
+    yards_score = (stats["PassingYards"].to_i / 25) * league.pass_yards
+    int_score = stats["PassingInterceptions"].to_i * league.int
+    two_pt_score = stats["TwoPointConversionPasses"].to_i * league.pass_2pt
     total = td_score + yards_score + two_pt_score + int_score
     total
   end
 
   def rush_score(stats, league)
-    td = stats["RushingTouchdowns"].to_i
-    yards = stats["RushingYards"].to_i
-    two_pt = stats["TwoPointConversionRuns"].to_i
-    td_rule = league.rush_td
-    yard_rule = league.rush_yards
-    two_pt_rule = league.rush_2pt
-    td_score = td * td_rule
-    yards_score = (yards / 10) * yard_rule
-    two_pt_score = two_pt * two_pt_rule
+    td_score = stats["RushingTouchdowns"].to_i * league.rush_td
+    yards_score = (stats["RushingYards"].to_i / 10) * league.rush_yards
+    two_pt_score = stats["TwoPointConversionRuns"].to_i * league.rush_2pt
     total = td_score + yards_score + two_pt_score
     total
   end
 
   def rec_score(stats, league)
-    td = stats["ReceivingTouchdowns"].to_i
-    yards = stats["ReceivingYards"].to_i
-    two_pt = stats["TwoPointConversionReceptions"].to_i
-    td_rule = league.rec_td
-    yard_rule = league.rec_yards
-    two_pt_rule = league.rec_2pt
-    td_score = td * td_rule
-    yards_score = (yards / 10) * yard_rule
-    two_pt_score = two_pt * two_pt_rule
+    td_score = stats["ReceivingTouchdowns"].to_i * league.rec_td
+    yards_score = (stats["ReceivingYards"].to_i / 10) * league.rec_yards
+    two_pt_score = stats["TwoPointConversionReceptions"].to_i * league.rec_2pt
     total = td_score + yards_score + two_pt_score
     total
   end
 
   def kick_score(stats, league)
-    fg_made = stats["FieldGoalsMade"].to_i
     fg_miss = stats["FieldGoalsAttempted"].to_i - stats["FieldGoalsMade"].to_i
-    pat_made = stats["ExtraPointsMade"].to_i
     pat_miss = stats["ExtraPointsAttempted"].to_i - stats["ExtraPointsMade"].to_i
-    fgmade_rule = league.fg_made
-    fg_miss_rule = league.fg_missed
-    patmade_rule = league.pat_made
-    fgmade_score = fg_made * fgmade_rule
-    fgmiss_score = (fg_miss * fg_miss_rule) + (pat_miss * fg_miss_rule)
-    patmade_score = pat_made * patmade_rule
+    fgmade_score = stats["FieldGoalsMade"].to_i * league.fg_made
+    fgmiss_score = (fg_miss * league.fg_missed) + (pat_miss * league.fg_missed)
+    patmade_score = stats["ExtraPointsMade"].to_i * league.pat_made
     total = fgmade_score + fgmiss_score + patmade_score
     total
   end
