f9375b069b0a119bab1b773301ca6252f98ec831
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-06-26 10:28:00 -0500

    added secret token env variable for production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FantasySportsCalculator::Application.config.secret_token = '0e9d2373219f6ed6d57d8ef69f2c4a2ec50fa90fe4858360f3c1338b564270882988f4cd98714abb3bb899a001176133b8e90c43923b2367379c424d004d26de'
+if Rails.env.development? or Rails.env.test?
+  FantasySportsCalculator::Application.config.secret_token = '0e9d2373219f6ed6d57d8ef69f2c4a2ec50fa90fe4858360f3c1338b564270882988f4cd98714abb3bb899a001176133b8e90c43923b2367379c424d004d26de'
+else
+  FantasySportsCalculator::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
