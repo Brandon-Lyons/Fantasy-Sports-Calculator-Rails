385ddce3753a370e218ddbd71998a911683eb8f4
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-07-10 20:30:13 -0500

    added misc stats calculation

--- app/models/football_player.rb
+++ app/models/football_player.rb
@@ -15,7 +15,8 @@ class FootballPlayer < ActiveRecord::Base
     rush = rush_score(stats, league)
     rec = rec_score(stats, league)
     kick = kick_score(stats, league)
-    total = pass + rush + rec + kick
+    misc = misc_score(stats, league)
+    total = pass + rush + rec + kick + misc
     total
   end
 
@@ -52,5 +53,58 @@ class FootballPlayer < ActiveRecord::Base
     patmade_score = stats["ExtraPointsMade"].to_i * league.pat_made
     total = fgmade_score + fgmiss_score + patmade_score
     total
+  end
+
+  def kick_td(stats, league)
+    kick_stat = stats["KickReturnTouchdowns"]
+    kick_rule = league.kick_td
+    score = kick_stat * kick_rule
+    score
+  end
+
+  def fum_td(stats, league)
+    fum_stat = stats["FumbleReturnTouchdowns"]
+    fum_rule = league.fumb_td
+    score = fum_stat * fum_rule
+    score    
+  end
+
+  def punt_td(stats, league)
+    punt_stat = stats["PuntReturnTouchdowns"]
+    punt_rule = league.punt_td
+    score = punt_stat * punt_rule
+    score    
+  end
+
+  def int_td(stats, league)
+    int_stat = stats["InterceptionReturnTouchdowns"]
+    int_rule = league.int_td
+    score = int_stat * int_rule
+    score
+  end
+
+  def block_td(stats, league)
+    block_stat = stats["BlockedKickReturnTouchdowns"]
+    block_rule = league.block_kick_td
+    score = block_stat * block_rule
+    score
+  end
+
+  def fum_lost(stats, league)
+    fum_stat = stats["FumblesLost"]
+    fum_rule = league.fumb_lost
+    score = fum_stat * fum_rule
+    score    
+  end
+
+  def misc_score(stats, league)
+    kick = kick_td(stats, league)
+    fumtd = fum_td(stats, league)
+    punt = punt_td(stats, league)
+    int = int_td(stats, league)
+    block = block_td(stats, league)
+    fumlost = fum_lost(stats, league)
+    score = kick + fumtd + punt + int + block + fumlost
+    score 
   end
 end
